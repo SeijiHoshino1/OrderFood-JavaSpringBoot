2be9005db0ee8d1c03fe6a90489c1db36b4bc81e
SeijiHoshino1 <seijihoshino@outlook.com>
2025-09-30 09:02:09 -0300

    Initial commit

---
+++ src/main/java/io/github/SeijiHoshino1/order_food/OrderFoodProjectApplication.java
@@ -0,0 +1,13 @@
+package io.github.SeijiHoshino1.order_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFoodProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFoodProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/SeijiHoshino1/order_food/OrderFoodProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.SeijiHoshino1.order_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderFoodProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
